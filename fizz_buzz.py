2e9701906a537c94f72b634301830a0df63c95f3
Hendrichmugisha <hendrichmugisha@gmail.com>
2025-06-26 20:36:02 +0530

    this is just a fizzbuz program I found online

---
+++ fizz_buzz.py
@@ -0,0 +1,11 @@
+#This  is just a simple programming challenge from the internet
+
+for number in range(1,101):
+    if (number%3)==0 and (number%5)==0:
+        print('Fizzbuzz')
+    elif (number%3==0):
+        print("Fizz")
+    elif (number%5==0):
+        print('Buzz')
+    else:
+        print(number)
